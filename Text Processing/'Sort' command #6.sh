7dbdac0e40403f1d29f4fd750a56040bb9007ce3
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-04-16 02:09:37 +0600

    Add files via upload

---
+++ Text Processing/'Sort' command #6.sh
@@ -0,0 +1,8 @@
+#(a) -> name of the city
+#(b) -> average monthly temperature in Jan (in Fahreneit)
+#(c) -> average monthly temperature in April (in Fahreneit)
+#(d) -> the average monthly temperature in July (in Fahreneit)
+#(e) -> the average monthly temperature in October (in Fahreneit
+
+
+sort -t$'\t' -n -k2
